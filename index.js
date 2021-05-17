592dd210b7fff401a65052ebb622d01ea6b7dcc7
AhJayzZ <antony95070@gmail.com>
2021-05-18 00:55:10 +0800

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+var linebot = require('linebot')
+var express = require('express')
+
+var bot = linebot({
+    channelId: 1653887573,
+    channelSecret: '7e6117cb1d36b389c79a95994bb63965',
+    channelAccessToken: 'RkwD54gpuR39YS+FYVZJgcnODhWiCCFhBjFBqMdVo8MmYGioCDUP7mBJnnfzTh/bE4RYEXdy4gBCjzlxoxbatgbydcYLub6VpDq/akUUdtsi+Pxc67WLgHMIg2d9cIYlPRkrP08aX5p7oZLThtXccwdB04t89/1O/w1cDnyilFU=',
+
+})
+
+//-------------------------------------------------------------------------
+
+bot.on('message', function(event) {
+    console.log(event); //把收到訊息的 event 印出來看看
+});
+
+const app = express();
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
+
+//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+var server = app.listen(process.env.PORT || 8080, function() {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
---
+++ linebot_meme.py
@@ -0,0 +1,10 @@
+import time,os
+import random
+from bs4 import BeautifulSoup
+
+
+meme_url = 'https://memes.tw/maker?page='
+
+random_num =  int((random.random()) * 500)
+
+print (random_num)
