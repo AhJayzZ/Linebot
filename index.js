338fd0ad247b9809916cbebfa031870bdda7ccef
AhJayzZ <antony95070@gmail.com>
2021-05-19 02:20:50 +0800

    test

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ function drawcard(msg) {
 
             const parser = new DOMParser();
             // var htmlDoc = parser.parseFromString(body, 'text/html');
-            // var random_index = Math.floor(Math.random() * 20);
+            var random_index = Math.floor(Math.random() * 20);
             // var image_class_name = htmlDoc.getElementsByClassName('sensitive-content')[random_index].innerHTML
             var htmlDoc = parser.parseFromString(body, 'text/html');
             var image_class_name = htmlDoc.getElementsByClassName('img-fluid lazy')[random_index].outerHTML
