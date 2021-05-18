b0f2750847c199a2f96ddd3d385ebdf283cf7058
AhJayzZ <antony95070@gmail.com>
2021-05-18 18:53:07 +0800

    asshole

--- index.js
+++ index.js
@@ -81,9 +81,9 @@ function drawcard(msg) {
             const parser = new DOMParser();
             var htmlDoc = parser.parseFromString(body, 'text/html');
             var random_index = Math.floor(Math.random() * 20);
-            var image_class_name = htmlDoc.getElementsByClassName('sensitive-content')[random_index].getAttribute('data-src');
-            //var image_url = image_class_name.getElementsByClassName('img-fluid lazy').innerHTML
-            console.log('image class name:', image_class_name);
+            var image_class_name = htmlDoc.getElementsByClassName('sensitive-content')[random_index]
+            var image_url = image_class_name.getAttribute('src')
+            console.log('image class name:', image_url);
 
         });
 
