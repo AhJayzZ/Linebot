f742c8d887f2f0d0b5c9770a5f8b75ffad6a1cd6
AhJayzZ <antony95070@gmail.com>
2021-05-18 23:01:06 +0800

    luck

--- index.js
+++ index.js
@@ -86,7 +86,9 @@ function drawcard(msg) {
             const parser = new DOMParser();
             var htmlDoc = parser.parseFromString(body, 'text/html');
             var random_index = Math.floor(Math.random() * 20);
-            var image_class_name = htmlDoc.getElementsByClassName('sensitive-content')[random_index].getAttribute('src')
+            //var image_class_name = htmlDoc.getElementsByClassName('sensitive-content')[random_index].
+            var image_class_name = htmlDoc.getElementsByClassName('img-fluid lazy loaded')[random_index].getAttribute('src')
+
             console.log('image class name:', image_class_name);
 
         });
