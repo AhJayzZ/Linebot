4585255a04e608ca621fe39203c9c8fb40857bbf
AhJayzZ <antony95070@gmail.com>
2021-05-19 03:22:28 +0800

    please

--- index.js
+++ index.js
@@ -84,9 +84,10 @@ function drawcard(event, msg) {
             }
         }
 
+
         request(meme_url, myoption, function(error, response, body) {
             console.error('error:', error); // Print the error if one occurred
-            console.log('statusCode:', response & response.statusCode); // Print the response status code if a response was received
+            //console.log('statusCode:', response & response.statusCode); // Print the response status code if a response was received
             //console.log('body:', body);                               // Print the HTML for the Google homepage.
 
             const parser = new DOMParser();
