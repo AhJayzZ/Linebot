aa657180d5aceab4cffbdd5c06275d986370a119
AhJayzZ <antony95070@gmail.com>
2021-05-18 17:35:19 +0800

    asshole

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ function drawcard(msg) {
 
             const parser = new DOMParser();
             var htmlDoc = parser.parseFromString(body, 'text/html');
-            image_class_name = htmlDoc.getElementsByClassName('img-fluid lazy')
+            image_class_name = htmlDoc.getElementsByClassName('img-fluid lazy').values
             console.log(image_class_name)
         });
 
