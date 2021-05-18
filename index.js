4168805756491f517ac27eb917595768be294078
AhJayzZ <antony95070@gmail.com>
2021-05-19 04:01:13 +0800

    please

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ function drawcard(event, msg) {
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
                 //'Host': 'memeprod.sgp1.digitaloceanspaces.com',
-                'Accept-Encoding': 'gzip, deflate, br',
+                //'Accept-Encoding': 'gzip, deflate, br',
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
                 'Cache-Control': 'max-age=0',
                 'Accept-Language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6',
