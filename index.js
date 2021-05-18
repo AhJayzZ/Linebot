74f7c0ed042b07859f56cb8fe067bc81d5f68583
AhJayzZ <antony95070@gmail.com>
2021-05-19 03:15:47 +0800

    please

--- index.js
+++ index.js
@@ -79,6 +79,8 @@ function drawcard(event, msg) {
             method: 'GET',
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+                'Host': 'memeprod.sgp1.digitaloceanspaces.com',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             }
         }
 
@@ -98,7 +100,6 @@ function drawcard(event, msg) {
                 originalContentUrl: image_url,
                 previewImageUrl: image_url,
             }
-
             event.reply(image_msg).then(function() { event.reply(image_url) });
 
         })
