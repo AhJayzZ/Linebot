ceca7a1ab7d3db41e6199d4aad2bbda41f055cf7
AhJayzZ <antony95070@gmail.com>
2021-05-18 17:55:47 +0800

    asshole

--- index.js
+++ index.js
@@ -73,13 +73,13 @@ function drawcard(msg) {
         meme_url = 'https://memes.tw/wtf?page=' + String(ranmdom_num);
         console.log('memeUrl:', meme_url);
 
-        res = request(meme_url, function(error, response) {
+        res = request(meme_url, function(error, response, body) {
             console.error('error:', error); // Print the error if one occurred
             console.log('statusCode:', response & response.statusCode); // Print the response status code if a response was received
-            console.log('body:', response.body); // Print the HTML for the Google homepage.
+            //console.log('body:', body); // Print the HTML for the Google homepage.
 
             const parser = new DOMParser();
-            var htmlDoc = parser.parseFromString(response.body, 'text/html');
+            var htmlDoc = parser.parseFromString(res.text, 'text/xml');
             image_class_name = htmlDoc.getElementsByClassName('sensitive-content')
             console.log(image_class_name)
         });
