e3124dc48a46cbaeb3681ca03d605defdd7fe9c9
AhJayzZ <antony95070@gmail.com>
2021-05-18 22:33:41 +0800

    test

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ bot.on('message', function(event) {
         else if (msg.search('早安') != -1)
             event.reply('你媽死了知道不?');
         else if (msg.search('==') != -1 || msg.search('= =') != -1)
-            event.reply('幹你娘誰在打== 或 = = 試試看');
+            event.reply('幹你娘誰再打== 或 = =，我就打誰');
         else if (msg.search('可憐') != -1)
             event.reply('你全家才可憐');
 
@@ -86,7 +86,7 @@ function drawcard(msg) {
             const parser = new DOMParser();
             var htmlDoc = parser.parseFromString(body, 'text/html');
             var random_index = Math.floor(Math.random() * 20);
-            var image_class_name = htmlDoc.getElementsByClassName('sensitive-content')[random_index]
+            var image_class_name = htmlDoc.getElementsByClassName('sensitive-content')[random_index].childNodes[0].nodeName
             console.log('image class name:', image_class_name);
 
         });
