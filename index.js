3ae8ff38e9cde0c04396bed9feaa5e5687d947c2
AhJayzZ <antony95070@gmail.com>
2021-05-19 03:47:43 +0800

    please

--- index.js
+++ index.js
@@ -82,6 +82,7 @@ function drawcard(event, msg) {
                 //'Host': 'memeprod.sgp1.digitaloceanspaces.com',
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
                 'Cache-Control': 'max-age=0',
+                'Accept-Encoding': 'gzip, deflate, br',
             }
         }
 
