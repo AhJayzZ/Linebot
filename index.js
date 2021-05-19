d4011ca9adf735d5682072935a98d880bb0b9cc1
AhJayzZ <antony95070@gmail.com>
2021-05-20 00:15:54 +0800

    last

--- index.js
+++ index.js
@@ -137,7 +137,8 @@ function dcard_sex_draw(event) {
         if (!error & res.statusCode == 200) {
             url_data = JSON.stringify(data);
             for (var k = 0; k < limit; k++) {
-                dcard_sex_image_url.push(url_data[k].media.url[0])
+                if (url_data[k].media[0].url != "")
+                    dcard_sex_image_url.push(url_data[k].media[0].url);
             }
 
             console.log(dcard_sex_image_url);
