c1c43766463f26fe580df883768b41a03e6c1857
AhJayzZ <antony95070@gmail.com>
2021-05-20 01:10:29 +0800

    test

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ function dcard_sex_draw(event) {
             for (var k = 0; k < limit; k++) {
                 if (data[k].media != '') {
                     console.log(data[k].media)
-                    if (typeof(data[k].media[k]) != null) {
+                    if (typeof(data[k]) != null) {
                         dcard_sex_image_url.push(data[k].media[k])
                     }
                 }
