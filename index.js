9a38268b4aef67d55b7ee50086cb03da0562c119
AhJayzZ <antony95070@gmail.com>
2021-05-20 01:35:42 +0800

    last

--- index.js
+++ index.js
@@ -101,8 +101,8 @@ function dcard_sex_draw(event) {
         if (res.statusCode != 200)
             return console.log('Status code:', res.statusCode);
         if (!error & res.statusCode == 200) {
-            console.log('data[0].media', data[0].media)
-            console.log('data[1].media', data[1].media[1])
+            console.log('data[0]', data[0])
+            console.log('data[1].media:', data[1].media[0])
 
 
             for (var k = 0; k < limit; k++) {
