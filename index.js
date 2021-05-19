a683ec5fdff7cd928219638d46073c245f385472
AhJayzZ <antony95070@gmail.com>
2021-05-19 15:50:32 +0800

    last

--- index.js
+++ index.js
@@ -32,8 +32,15 @@ bot.on('message', function(event) {
         console.log('Recevied:', msg);
 
         // Draw a card
-        if (parseInt(msg.search('抽')) != -1)
+        if (parseInt(msg.search('大爆抽')) != -1) {
+            for (var i = 0; i < 10; i++)
+                drawcard(event, msg);
+        } else if (parseInt(msg.search('爆抽')) != -1) {
+            for (var i = 0; i < 5; i++)
+                drawcard(event, msg);
+        } else if (parseInt(msg.search('抽')) != -1)
             drawcard(event, msg);
+
 
 
         for (i = 0; i < name.length; i++) {
