9ca4918822fb4b061bc5cca5c2767c16229db5d6
AhJayzZ <antony95070@gmail.com>
2021-05-19 15:13:56 +0800

    last

--- index.js
+++ index.js
@@ -86,15 +86,18 @@ function drawcard(event, msg) {
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
             //'Host': 'memeprod.sgp1.digitaloceanspaces.com',
-            'Accept-Encoding': 'gzip, deflate',
+            //'Accept-Encoding': 'gzip, deflate,br',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             'Cache-Control': 'max-age=0',
             'Accept-Language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6',
             'Connection': 'keep-alive',
+            'sec-ch-ua: " Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
             'Sec-Fetch-Dest': 'document',
             'Sec-Fetch-Mode': 'navigate',
             'Sec-Fetch-Site': 'none',
             'Sec-Fetch-User': '?1',
+            'Upgrade-Insecure-Requests': '1',
+            'Referer': 'https://memeprod.sgp1.digitaloceanspaces.com/user-wtf/1621257098763.jpg',
         }
     }
 
