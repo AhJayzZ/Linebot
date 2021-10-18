d2e672cdbcf9c1c2dab9bd5e6b0f3450f201b8b0
AhJayzZ <antony95070@gmail.com>
2021-10-18 19:56:30 +0800

    meme video test

--- index.js
+++ index.js
@@ -228,8 +228,8 @@ function meme_video(event) {
             const parser = new DOMParser();
             var random_index = Math.floor(Math.random() * 20);
             var htmlDoc = parser.parseFromString(body, 'text/html');
-            var videoClassName = htmlDoc.getElementsByClassName('_26gD _3_Go');
-            console.log(videoClassName)
+            var videoClassName = htmlDoc.getElementsByClassName('_26gD _3_Go')[0];
+            console.log(videoClassName.innerHTML)
         }
 
         // //Step 2.Get mp4 in video url
